3620614e12727448dc9fac262ac7a14b57af56cf
vanessamaria11 <vanessamariaotto@gmail.com>
2017-05-12 21:20:41 -0400

    edited files

--- teacherSequelize/routes/html-routes.js
+++ teacherSequelize/routes/html-routes.js
@@ -8,54 +8,76 @@ var multer  = require('multer')
 
 module.exports = function(app) {
 
-	// Each of the below routes just handles the HTML page that the user gets sent to.
+  // Each of the below routes just handles the HTML page that the user gets sent to.
 
-	// index route loads view.html
+  // index route loads view.html
 
-app.get("/", function(req, res){(
+app.get("/", function(req, res){
 
-	db.image.findAll({}).then(function(images) {
+  db.image.findAll({}).then(function(images) {
                   //  Promise.all(
                   // console.log (results.get({plain:true}));
                  var imageArr=[];
                  for(var i = 0; i< images.length; i++){
-                 	imageArr.push(images[i].get({plain:true}));
+                  imageArr.push(images[i].get({plain:true}));
                  }
-			res.render("index")
-		})
+      res.render("index")
+    })
 
-)});
+});
 
-app.get("/auction", function(req, res){(
+//route for auction page loads images if users or images don't exist in db
+app.get("/auction", function(req, res){
 
-	db.image.findAll({ include: [db.User] }).then(function(images) {
-                  //  Promise.all(
-                  // console.log (results.get({plain:true}));
-                 var imageArr=[];
-                 for(var i = 0; i< images.length; i++){
-                 	imageArr.push(images[i].get({plain:true}));
-                 }
-			res.render("auction", {imageArr: imageArr})
-		})
+  db.User.findAll({}).then(function(results) {
+    // res.json(results);
 
-)});
+    if (results == null) {
+          db.image.findAll({}).then(function(images) {
+            //  Promise.all(
+            // console.log (results.get({plain:true}));
+            if (images == null) {
+              res.render("auction");
+            }
+           var imageArr=[];
+           for(var i = 0; i< images.length; i++){
+            imageArr.push(images[i].get({plain:true}));
+           }
+          res.render("auction", {imageArr: imageArr})
+          });
+    }
+    else {
+          db.image.findAll({ include: [db.User] }).then(function(images) {
+            //  Promise.all(
+            // console.log (results.get({plain:true}));
+            console.log()
+            var imageArr=[];
+            for(var i = 0; i< images.length; i++){
+             imageArr.push(images[i].get({plain:true}));
+            }
+            res.render("auction", {imageArr: imageArr})
+          });
+    }
+  });
+
+});
 
   app.get("/register", function(req, res) {
 
-  	res.render("register");
+    res.render("register");
 
 });
 
   app.get("/auction", function(req, res) {
 
 
-  	res.render("auction");
+    res.render("auction");
 
 });
 
   app.get("/home", function(req, res) {
 
-  	res.render("home");
+    res.render("home");
 
 });
 
--- teacherSequelize/routes/item-routes.js
+++ teacherSequelize/routes/item-routes.js
@@ -11,62 +11,55 @@ var multer = require('multer');
 
 module.exports = function(app) {
 
-	app.get("/auction/image", function(req, res){
+  //route to retrieve all the images from the db
+  app.get("/api/image", function(req, res){
 
-		db.image.findAll({}).then(function(result){
-			// console.log(result[0].image);
-			var hbsObject = {
-				image: result
-			}
-			// console.log(result[0].dataValues.image);
-			res.json(result);
-			// res.render("auction", hbsObject);
+    db.image.findAll({}).then(function(result){
+      // console.log(result[0].image);
+      // var hbsObject = {
+      //  image: result
+      // }
+      res.json(result);
+      // res.render("auction", hbsObject);
+    });
+  }); //end /auction/image route
 
-		});
-	});
+  //get request route to retrieve the selected picture's bid price
+  app.get("/auction/getprice:id", function(req, res){
 
-	app.get("/api/getprice:id", function(req, res){
+    console.log(req.params.id);
+    db.image.findOne({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(results) {
 
-		console.log(req.params.id);
-  	db.image.findOne({
-  		where: {
-  			id: req.params.id
-  		}
-  	}).then(function(results) {
+      // console.log(results.bidprice);
+      res.json(results);
+    });
+  }); //end /api/getprice:id route
 
-  		console.log(results.bidprice);
-  		res.json(results);
+  //route retrieves specific token to validate which user posted
+  app.get("/auction/checklog:token", function(req, res) {
+    db.User.findOne({
+      where: {
+        token: req.params.token
+      }
+    }).then(function(results) {
+      // console.log(results.login);
+        res.json(results);
+    })
+  }); //end /api/checklog:token route
 
-  	});
-
-	});
-
-	app.get("/api/checklog:token", function(req, res) {
-		db.User.findOne({
-			where: {
-				token: req.params.token
-			}
-		}).then(function(results) {
-			console.log(results.login);
-  			res.json(results);
-		})
-	});
-
-
-app.post("/api/bid", function(req, res){
-
-        console.log(req.body.getToken);
-        console.log(req.body.getToken);
-        console.log(req.body.pictureNum);
+  //post is made to route to find the user who made the bid and then update the price
+  //in the image db
+  app.post("/auction/bid", function(req, res){
 
         db.User.findOne({
           where: {
               token: req.body.getToken
           }
       }).then(function(result) {
-
-         console.log(result.userName);
-
 
             db.image.update({bidprice: req.body.bidAmount, UserId: result.id}, 
             {
@@ -75,21 +68,21 @@ app.post("/api/bid", function(req, res){
                 }
             }).then(function(results) {
 
-            	console.log(results);
-  //           		db.image.findAll({}).then(function(images) {
-  //                 //  Promise.all(
-  //                 // console.log (results.get({plain:true}));
-  //                var imageArr=[];
-  //                for(var i = 0; i< images.length; i++){
-  //                	imageArr.push(images[i].get({plain:true}));
-  //                }
-		// 	// res.render("auction", {imageArr: imageArr})
-		// })
-                res.redirect("/auction");
-            })
+              console.log("last console log before return");
+              console.log(results);
+                db.image.findAll({}).then(function(images) {
 
+                  var imageArr=[];
+                  for(var i = 0; i< images.length; i++){
+                    imageArr.push(images[i].get({plain:true}));
+                  }
+                  console.log("we made it to images return");
+                  console.log(images);
+                  res.json(images);
+                }) //end db.image.findAll search
+            })
      });
 
-    });
+  }); //end /api/bid route
 
-}
+} // end modual exports
